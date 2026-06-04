3f72847490d36d7dd0d0646d8ede14a85b050056
sinasamaki <sinasamaki@protonmail.com>
2026-06-04 11:24:50 +0200

    update thanks slide

--- composeApp/src/desktopMain/kotlin/com/sinasamaki/chromadecks/_talks/ui_delight/slides/ThankYouTitleCard.kt
+++ composeApp/src/desktopMain/kotlin/com/sinasamaki/chromadecks/_talks/ui_delight/slides/ThankYouTitleCard.kt
@@ -1,15 +1,28 @@
 package com.sinasamaki.chromadecks._talks.ui_delight.slides
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import chromadecks.composeapp.generated.resources.Res
+import coil3.compose.AsyncImage
 import com.sinasamaki.chromadecks._talks.ui_delight.components.TitleCardFrame
 import com.sinasamaki.chromadecks.data.ListSlideAdvanced
-import com.sinasamaki.chromadecks.ui.theme.Orange600
-import com.sinasamaki.chromadecks.ui.theme.Purple600
-import com.sinasamaki.chromadecks.ui.theme.Yellow500
-import com.sinasamaki.chromadecks.ui.theme.Yellow600
+import com.sinasamaki.chromadecks.ui.components.Space
 import com.sinasamaki.chromadecks.ui.theme.Zinc300
 import com.sinasamaki.chromadecks.ui.theme.Zinc700
-import com.sinasamaki.chromadecks.ui.theme.Zinc800
 import com.sinasamaki.chromadecks.ui.theme.Zinc950
 
 class ThankYouTitleCardState
@@ -20,11 +33,45 @@ class ThankYouTitleCard: ListSlideAdvanced<ThankYouTitleCardState>() {
 
     @Composable
     override fun content(state: ThankYouTitleCardState) {
-        TitleCardFrame(
-            title = "Thank you!",
-            description = "",
-            backgroundColor = Zinc950,
-            borderColor = Zinc700,
-        )
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            TitleCardFrame(
+                title = "Thank you!",
+                description = "",
+                backgroundColor = Zinc950,
+                borderColor = Zinc700,
+            )
+            Row(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(64.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                AsyncImage(
+                    model = Res.getUri("drawable/myheart.png"),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(120.dp)
+                        .clip(CircleShape)
+                )
+                Space(32.dp)
+                Column {
+                    Text(
+                        text = "sinasamaki.com",
+                        style = MaterialTheme.typography.labelLarge.copy(fontSize = 48.sp),
+                        color = Zinc300,
+                    )
+                    Space(2.dp)
+                    Text(
+                        text = "@sinasamaki",
+                        style = MaterialTheme.typography.labelLarge.copy(fontSize = 32.sp),
+                        color = Zinc300,
+                    )
+                }
+            }
+        }
+
     }
 }
